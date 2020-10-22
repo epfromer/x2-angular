374bf63355f5ef22ef2065b9db811e91725be9a7
Ed Pfromer <epfromer@gmail.com>
2020-10-22 15:05:49 -0600

    update

---
+++ packages/web/src/app/app-toolbar.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './app-toolbar.component.html',
+})
+export class AppToolbarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- packages/web/src/app/app.module.ts
+++ packages/web/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { ToolbarComponent } from './toolbar.component'
+import { AppToolbarComponent } from './app-toolbar.component'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
-  declarations: [AppComponent, ToolbarComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [AppComponent, AppToolbarComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSliderModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
