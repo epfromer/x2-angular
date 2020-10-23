907732da76e19873a40e0cb14afc1b037da522e6
Ed Pfromer <epfromer@gmail.com>
2020-10-22 19:23:58 -0600

    update

--- packages/web/src/app/app.module.ts
+++ packages/web/src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
 import { MatSliderModule } from '@angular/material/slider'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { BrowserModule } from '@angular/platform-browser'
@@ -6,7 +8,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module'
 import { AppToolbarComponent } from './app-toolbar.component'
 import { AppComponent } from './app.component'
-import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
   declarations: [AppComponent, AppToolbarComponent],
@@ -15,6 +16,7 @@ import { MatButtonModule } from '@angular/material/button'
     BrowserAnimationsModule,
     BrowserModule,
     MatButtonModule,
+    MatIconModule,
     MatSliderModule,
     MatToolbarModule,
   ],
