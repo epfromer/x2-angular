dcea93a8be6779fc60204f90de1318cca7742e8a
Ed Pfromer <epfromer@gmail.com>
2020-10-23 09:36:45 -0600

    update

--- packages/web/src/app/app-toolbar.component.ts
+++ packages/web/src/app/app-toolbar.component.ts
@@ -1,11 +1,23 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './app-toolbar.component.html',
 })
-export class AppToolbarComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
+export class AppToolbarComponent {
+  onDrawer(): void {
+    console.log('drawer')
+  }
+  onDark(): void {
+    console.log('dark')
+  }
+  onSettings(): void {
+    console.log('settings')
+  }
+  onSearch(): void {
+    console.log('search')
+  }
+  onHome(): void {
+    console.log('home')
+  }
 }
