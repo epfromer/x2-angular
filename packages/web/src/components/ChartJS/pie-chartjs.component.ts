8ac87979ca5529174e81f51b9c28cffb57ab8a84
Ed Pfromer <epfromer@gmail.com>
2020-10-29 07:32:40 -0600

    update

--- packages/web/src/components/ChartJS/pie-chartjs.component.ts
+++ packages/web/src/components/ChartJS/pie-chartjs.component.ts
@@ -1,6 +1,13 @@
 import { Component, Input } from '@angular/core'
 import { Router } from '@angular/router'
 import { EmailXferedDatum } from '../../store/types'
+import { ChartType, ChartOptions } from 'chart.js'
+import {
+  SingleDataSet,
+  Label,
+  monkeyPatchChartJsLegend,
+  monkeyPatchChartJsTooltip,
+} from 'ng2-charts'
 
 @Component({
   selector: 'pie-chartjs',
@@ -13,8 +20,25 @@ export class PieChartJSComponent {
   @Input() handleClick: (search: string, name: string) => void
 
   // eslint-disable-next-line prettier/prettier
-  constructor(private _router: Router) { }
+  constructor(private _router: Router) {
+    monkeyPatchChartJsTooltip()
+    monkeyPatchChartJsLegend()
+  }
+
+  public chartHeight = '350'
 
   // https://www.npmjs.com/package/ng2-charts
 
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+  }
+  public pieChartLabels: Label[] = [
+    ['Download', 'Sales'],
+    ['In', 'Store', 'Sales'],
+    'Mail Sales',
+  ]
+  public pieChartData: SingleDataSet = [300, 500, 100]
+  public pieChartType: ChartType = 'pie'
+  public pieChartLegend = true
+  public pieChartPlugins = []
 }
