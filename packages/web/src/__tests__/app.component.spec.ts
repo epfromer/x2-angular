c0d7873201337533c50c93bf78eb156a56316508
Ed Pfromer <epfromer@gmail.com>
2020-11-25 12:42:04 -0700

    update

---
+++ packages/web/src/__tests__/app.component.spec.ts
@@ -0,0 +1,69 @@
+import { HttpClientModule } from '@angular/common/http'
+import { TestBed } from '@angular/core/testing'
+import { MatDividerModule } from '@angular/material/divider'
+import { MatIconModule } from '@angular/material/icon'
+import { MatListModule } from '@angular/material/list'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { RouterTestingModule } from '@angular/router/testing'
+import { AuthModule } from '@auth0/auth0-angular'
+import { MockStore, provideMockStore } from '@ngrx/store/testing'
+import { AppComponent } from 'src/app.component'
+import { AppDrawerComponent } from 'src/components/app/app-drawer.component'
+import { AppToolbarComponent } from 'src/components/app/app-toolbar.component'
+import { SettingsButtonComponent } from 'src/components/app/settings-button.component'
+import { HomeCardComponent } from 'src/components/home-card.component'
+import { environment } from 'src/environments/environment'
+import { AppRoutingModule } from 'src/router/app-routing.module'
+
+// https://ngrx.io/guide/store/testing
+
+describe('AppComponent', () => {
+  let store: MockStore
+  const initialState = {}
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        BrowserModule,
+        HttpClientModule,
+        AppRoutingModule,
+        AuthModule.forRoot({
+          domain: environment.auth0Domain,
+          clientId: environment.auth0ClientId,
+        }),
+        BrowserAnimationsModule,
+        MatDividerModule,
+        MatIconModule,
+        MatListModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        RouterTestingModule,
+      ],
+      declarations: [
+        AppComponent,
+        AppDrawerComponent,
+        AppToolbarComponent,
+        HomeCardComponent,
+        SettingsButtonComponent,
+      ],
+      providers: [provideMockStore(initialState)],
+    }).compileComponents()
+
+    store = TestBed.inject(MockStore)
+  })
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.componentInstance
+    expect(app).toBeTruthy()
+  })
+
+  it(`should have as title 'x2 Angular'`, () => {
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.componentInstance
+    expect(app.title).toEqual('x2 Angular')
+  })
+})
